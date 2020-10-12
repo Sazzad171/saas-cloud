d7a08a1a5e80faf69d1d1ccbafd70a7502d74d2b
Sazzad171 <sajjad.cse.diu@gmail.com>
2020-10-12 12:10:43 +0600

    some update

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,6 @@
 (function ($) {
 "use strict";
 
-
 $('.owl-theme').owlCarousel({
 	items: 3,
 	loop:true,
@@ -31,50 +30,42 @@ $('.owl-theme').owlCarousel({
 			items:3
 		}
 	}
-})
-
-
-/* magnificPopup img view */
-$('.popup-image').magnificPopup({
-	type: 'image',
-	gallery: {
-	  enabled: true
-	}
 });
 
-/* magnificPopup video view */
-$('.popup-video').magnificPopup({
-	type: 'iframe'
+// slick slider for company logo
+$('.responsive').slick({
+	dots: false,
+	infinite: true,
+	speed: 1500,
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 1000,
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: false
+			}
+		},
+		{
+			breakpoint: 667,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 400,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		},
+	]
 });
-
-
-
-$('.grid').imagesLoaded( function() {
-	// init Isotope
-	var $grid = $('.grid').isotope({
-	  itemSelector: '.grid-item',
-	  percentPosition: true,
-	  masonry: {
-		// use outer width of grid-sizer for columnWidth
-		columnWidth: '.grid-item',
-	  }
-	});
-});
-
-// filter items on button click
-$('.portfolio-menu').on( 'click', 'button', function() {
-  var filterValue = $(this).attr('data-filter');
-  $grid.isotope({ filter: filterValue });
-});	
-
-
-
-//for menu active class
-$('.portfolio-menu button').on('click', function(event) {
-	$(this).siblings('.active').removeClass('active');
-	$(this).addClass('active');
-	event.preventDefault();
-});
-
 
 })(jQuery);	
